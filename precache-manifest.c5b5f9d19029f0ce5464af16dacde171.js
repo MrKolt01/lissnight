1937f7838fab64988110e2c9313041cce96db04d
mrkolt01 <kolt.zar@mail.ru>
2020-08-20 18:31:06 +0300

    Updates

---
+++ precache-manifest.c5b5f9d19029f0ce5464af16dacde171.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8a64601babec9306005520e30a3423f",
+    "url": "/lissnight/index.html"
+  },
+  {
+    "revision": "e75ca695c51ddaa704e1",
+    "url": "/lissnight/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "c509ac8bc6a0804b6e10",
+    "url": "/lissnight/static/css/main.a3341fcc.chunk.css"
+  },
+  {
+    "revision": "e75ca695c51ddaa704e1",
+    "url": "/lissnight/static/js/2.da332650.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/lissnight/static/js/2.da332650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c509ac8bc6a0804b6e10",
+    "url": "/lissnight/static/js/main.d19cfe04.chunk.js"
+  },
+  {
+    "revision": "58a04dba0744176ca013",
+    "url": "/lissnight/static/js/runtime-main.c821623a.js"
+  }
+]);
